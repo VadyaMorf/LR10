4d068671b49b804ddf67a7ec44c67a010c68ba3a
VadyaMorf <152396090+VadyaMorf@users.noreply.github.com>
2024-11-11 01:47:23 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img:
+      'https://imei.org/storage/files/images/7754/preview/samsung-galaxy-s7-3.png',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img:
+      'https://pixophone.com/image/cache/catalog/product-upload/thumb_828686_default_big-567x517.jpg',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img:
+      'https://i.allo.ua/media/catalog/product/cache/1/image/710x600/602f0fa2c1f0d1ba5e241f914e856ff9/x/i/xiaomi_mi_note_2_128gb_silver.webp',
+    amount: 1,
+  },
+];
